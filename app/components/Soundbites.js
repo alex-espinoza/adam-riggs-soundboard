536c6834d20dac2973ed39748994cb314a52eeb0
Alex Espinoza <aespinoza@aspria.net>
2016-03-02 16:33:45 -0500

    Update soundbite data with test mp3 audio files

--- app/components/Soundbites.js
+++ app/components/Soundbites.js
@@ -6,16 +6,16 @@ var Soundbites = React.createClass({
   getInitialState: function() {
     return {
       soundbites: [
-        {'label': 'Awesome', 'src': 'http://www.google.com'},
-        {'label': 'Fantastic', 'src': 'http://www.google.com'},
-        {'label': 'Great', 'src': 'http://www.google.com'},
-        {'label': 'Mm mm mm', 'src': 'http://www.google.com'},
-        {'label': 'So I Used To Date This Gal', 'src': 'http://www.google.com'},
-        {'label': 'Uhhhhhh', 'src': 'http://www.google.com'},
-        {'label': 'Okay', 'src': 'http://www.google.com'},
-        {'label': 'Nothing', 'src': 'http://www.google.com'},
-        {'label': 'Test', 'src': 'http://www.google.com'},
-        {'label': 'Oh my god', 'src': 'http://www.google.com'},
+        {'label': 'Awesome', 'src': 'http://www.noiseaddicts.com/samples_1w72b820/2247.mp3', id: 'awesome'},
+        {'label': 'Fantastic', 'src': 'http://www.noiseaddicts.com/samples_1w72b820/2247.mp3', id: 'fantastic'},
+        {'label': 'Great', 'src': 'http://www.noiseaddicts.com/samples_1w72b820/2247.mp3', id: 'great'},
+        {'label': 'Mm mm mm', 'src': 'http://www.noiseaddicts.com/samples_1w72b820/2247.mp3', id: 'mm'},
+        {'label': 'So I Used To Date This Gal', 'src': 'http://www.noiseaddicts.com/samples_1w72b820/2247.mp3', id: 'so'},
+        {'label': 'Uhhhhhh', 'src': 'http://www.noiseaddicts.com/samples_1w72b820/2247.mp3', id: 'uhh'},
+        {'label': 'Okay', 'src': 'http://www.noiseaddicts.com/samples_1w72b820/2247.mp3', id: 'okay'},
+        {'label': 'Nothing', 'src': 'http://www.noiseaddicts.com/samples_1w72b820/2247.mp3', id: 'nothing'},
+        {'label': 'Test', 'src': 'http://www.noiseaddicts.com/samples_1w72b820/2247.mp3', id: 'test'},
+        {'label': 'Oh my god', 'src': 'http://www.noiseaddicts.com/samples_1w72b820/2247.mp3', id: 'oh'},
       ]
     }
   },
